6d64f0d24885622b42e30ee19fd46f1660ec741a
choisuyang <chltndid724@gmail.com>
2021-08-02 18:01:18 +0900

    shop-detail

--- static/js/common.js
+++ static/js/common.js
@@ -2,6 +2,8 @@ function getCookie(cname) {
   var name = cname + "=";
   var decodedCookie = decodeURIComponent(unescape(document.cookie));
   var ca = decodedCookie.split(";");
+  console.log("----> decodeCookie", decodedCookie);
+  console.log("----> ca", ca);
   for (var i = 0; i < ca.length; i++) {
     var c = ca[i];
     while (c.charAt(0) == " ") {
@@ -18,6 +20,7 @@ function setCookieHour(name, value, hours) {
   var now = new Date();
   var time = now.getTime();
   time += 3600 * 1000 * hours;
+  console.log("---->Time", time);
   now.setTime(time);
   document.cookie = name + "=" + escape(value) + ";path/; expires=" + now.toUTCString() + ";";
 }
